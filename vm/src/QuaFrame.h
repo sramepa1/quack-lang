702660d6f42a1705d1e1e180520cb830fb31b264
Ota J. Sandr <ota@R-U-K-A.(none)>
2012-10-29 15:35:13 +0100

    Added macros for simple accessing to nested members of struct QuaFrame

--- vm/src/QuaFrame.h
+++ vm/src/QuaFrame.h
@@ -1,6 +1,10 @@
 #ifndef _QUA_FRAME_H_ 
 #define _QUA_FRAME_H_
 
+extern "C" {
+    #include <stdint.h>
+}
+
 #pragma pack(1)
 
 #define EXIT 0
@@ -8,37 +12,56 @@
 #define EXCEPTION 2
 #define FINALLY 3
 
+#define FRAME_TYPE type.frameType
+#define INTERPRETED type.interpreted
+
+#define ARG_COUNT typeData.methodFrame.argCount
+#define DEST_REG typeData.methodFrame.destReg
+#define BP_OFFSET typeData.methodFrame.bpOffset
+
+#define EXCEPTION_TYPE typeData.exceptionType
+
 struct QuaFrame {
-	
-	void* code;
-	struct {
-		unsigned frameType : 2;
-		unsigned interpreted : 1;
-		unsigned : 5;
-	} type;
-	
-	union {
-		struct {
-			char argCount;
-			uint16_t destReg;
-			uint32_t bpOffset;
-		} methodFrame;
-	
-		uint16_t exceptionType;
-	} typeData;
-	
-	// creates method frame
-	QuaFrame(void* code, bool interpreted, char argCount, uint16_t destReg, uint32_t bpOffset)
-		: code(code), type.frameType(METHOD), type.intepreted(interpreted), typeData.methodFrame.argCount(argCount),
-		  typeData.methodFrame.destReg(destReg), typeData.methodFrame.bpOffset(bpOffset) {}
-	
-	// creates exception handler
-	QuaFrame(void* code, bool interpreted, uint16_t exceptionType) : code(code), type.frameType(EXCEPTION),
-		type.interpreted(interpreted), typeData.exceptionType(exceptionType) {}
-	
-	// creates finally or exit handler
-	QuaFrame(void* code, bool interpreted, bool finally) : code(code), type.typeFrame(finally ? FINALLY : EXIT),
-		type.interpreted(interpreted) {}
+
+    void* code;
+    struct {
+        unsigned frameType : 2;
+        unsigned interpreted : 1;
+        unsigned : 5;
+    } type;
+
+    union {
+        struct {
+            char argCount;
+            uint16_t destReg;
+            uint32_t bpOffset;
+        } methodFrame;
+    
+        uint16_t exceptionType;
+    } typeData;
+
+    // creates method frame
+    QuaFrame(void* code, bool interpreted, char argCount, uint16_t destReg, uint32_t bpOffset) : code(code) {
+        FRAME_TYPE = METHOD;
+        INTERPRETED = interpreted;
+        ARG_COUNT = argCount;
+        DEST_REG = destReg;
+        BP_OFFSET = bpOffset;
+    }
+
+    // creates exception handler
+    QuaFrame(void* code, bool interpreted, uint16_t exceptionType) : code(code) {
+        FRAME_TYPE = EXCEPTION;
+        INTERPRETED = interpreted;
+        EXCEPTION_TYPE = exceptionType;
+    }
+
+    // creates finally or exit handler (it depends on third parameter)
+    QuaFrame(void* code, bool interpreted, bool finally) : code(code) {
+        FRAME_TYPE = finally ? FINALLY : EXIT;
+        INTERPRETED = interpreted;
+    }
+
 };
 
 #pragma pack()
